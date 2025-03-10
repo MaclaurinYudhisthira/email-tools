66b6117c25e37ee4187a76805293066e6543208c
Kishan Dasondhi <kishan@switchon.io>
2025-03-10 17:24:46 +0530

    Clean up

--- email_verifier/frontend/src/App.jsx
+++ email_verifier/frontend/src/App.jsx
@@ -4,9 +4,7 @@ import Signup from "./pages/SignUp";
 import VerifyOTP from "./pages/VerifyOTP";
 import Login from "./pages/Login";
 import Upload from "./pages/Upload";
-import Download from "./pages/Download";
 import Landing from "./pages/Landing";
-import Header from "./components/Header";
 // import "./App.css";
 
 function App() {
@@ -37,36 +35,3 @@ function App() {
 }
 
 export default App;
-// 
-// {/* <div className="min-h-screen flex items-center justify-center bg-gray-100 app-container">
-// <div className="w-4/5 max-w-4xl p-6 bg-white shadow-lg rounded-xl mx-auto">
-// {isAuthenticated && (
-//     <button onClick={handleLogout} className="absolute top-4 right-4 bg-red-500 text-white px-4 py-2 rounded">Logout</button>
-//   )}
-//   <Routes>
-//     <Route path="/signup" element={
-      
-//       <div className="flex flex-col items-center">
-//           <Signup />
-//           <p className="mt-4 text-gray-600">
-//             Already have an account? <a href="/login" className="text-blue-500 hover:underline">Log in</a>
-//           </p>
-//         </div>
-//       } />
-//     
-//     <Route path="/login" element={
-//       isAuthenticated ? <Navigate to="/upload" /> : (
-//         <div className="flex flex-col items-center">
-//           <Login onLoginSuccess={() => setIsAuthenticated(true)} />
-//           <p className="mt-4 text-gray-600">
-//             Don't have an account? <a href="/signup" className="text-blue-500 hover:underline">Sign Up</a>
-//           </p>
-//         </div>
-//       )
-//     } />
-//     <Route path="/upload" element={isAuthenticated ? <Upload /> : <Navigate to="/login" />} />
-//     {/* <Route path="/download" element={isAuthenticated ? <Download /> : <Navigate to="/login" />} /> */}
-//     <Route path="*" element={<Navigate to="/login" />} />
-//   </Routes>
-// </div>
-// </div> */}
